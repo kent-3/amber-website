ab92e48b21a1062ecb4299824adddffb31fe8155
kent-3 <100624004+kent-3@users.noreply.github.com>
2022-10-16 20:52:43 -0400

    add some mobile compatibility

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,43 @@ import { setupConnectWallet } from "./connect-wallet";
 setupConnectKeplr(document.querySelector<HTMLButtonElement>('#connect-keplr')!)
 setupConnectWallet(document.querySelector<HTMLButtonElement>('#connect-wallet')!)
 
+// Get the modal
+var modal = document.querySelector<HTMLElement>('#myModal')
+// var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.querySelector<HTMLButtonElement>('#start-button')
+
+var help = document.querySelector<HTMLButtonElement>('#help')
+var helpText = document.querySelector<HTMLElement>('#helptext')
+help.onclick = () => {
+  if (helpText.style.visibility == "hidden") {
+    helpText.style.visibility = "visible"
+  } else {
+    helpText.style.visibility = "hidden"
+  }
+}
+
+// Get the <span> element that closes the modal
+var span = document.querySelector<HTMLButtonElement>('#close')
+
+// When the user clicks on the button, open the modal
+btn.onclick = () => {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
 const grpcWebUrl = import.meta.env.VITE_MAINNET_GRPC_URL
 const chainId = import.meta.env.VITE_MAINNET_CHAIN_ID
 
@@ -20,35 +57,9 @@ console.log(`AmberDAO has ${scrt} SCRT staked.`)
 document.querySelector<HTMLElement>('#stake-value').innerHTML=`${scrt}`
 document.querySelector<HTMLElement>('#voting-power').innerHTML=`${scrt}`
 
-const { delegationResponses: delegationResponse } = await secretjs.query.staking.validatorDelegations({validatorAddr: 'secretvaloper18w7rm926ue3nmy8ay58e3lc2nqnttrlhhgpch6', pagination: {limit:'1000000'}})
+const { delegationResponses: delegationResponse } = await secretjs.query.staking.validatorDelegations({validatorAddr: 'secretvaloper18w7rm926ue3nmy8ay58e3lc2nqnttrlhhgpch6', pagination: {limit:'10000'}})
 let total: number = delegationResponse.length
 console.log(`AmberDAO has ${total} delegations.`)
 document.querySelector<HTMLElement>('#delegators').innerHTML=`${total}`
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("start-button");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-// @ts-ignore
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
