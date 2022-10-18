9ea32ac9215d37d66f1319617feee0c67d14fcc2
kent-3 <100624004+kent-3@users.noreply.github.com>
2022-10-17 20:55:45 -0400

    positioning things *just* right

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,19 @@ setupConnectWallet(document.querySelector<HTMLButtonElement>('#connect-wallet')!
 setupSuggestToken(document.querySelector<HTMLElement>('#airdrop-box')!)
 setupClaim(document.querySelector<HTMLButtonElement>('#claim-button')!)
 
-// Get the modal
 var modal = document.querySelector<HTMLElement>('#myModal')
-// var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.querySelector<HTMLButtonElement>('#start-button')
-
+var open = document.querySelector<HTMLButtonElement>('#start-button')
+var close = document.querySelector<HTMLButtonElement>('#close')
 var help = document.querySelector<HTMLButtonElement>('#help')
 var helpText = document.querySelector<HTMLElement>('#helptext')
+
+
+open.onclick = () => {
+  modal.style.display = "block";
+}
+close.onclick = function() {
+  modal.style.display = "none";
+}
 help.onclick = () => {
   if (helpText.style.visibility == "hidden") {
     helpText.style.visibility = "visible"
@@ -26,47 +30,29 @@ help.onclick = () => {
   }
 }
 
-// Get the <span> element that closes the modal
-var span = document.querySelector<HTMLButtonElement>('#close')
-
-// When the user clicks on the button, open the modal
-btn.onclick = () => {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
-
-const grpcWebUrl = import.meta.env.VITE_MAINNET_GRPC_URL
-const chainId = import.meta.env.VITE_MAINNET_CHAIN_ID
-
-
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// } 
 
 window.onload = async ()=>{
-
+  const grpcWebUrl = import.meta.env.VITE_MAINNET_GRPC_URL
+  const chainId = import.meta.env.VITE_MAINNET_CHAIN_ID
   const secretjs = await SecretNetworkClient.create({
     grpcWebUrl,
-    chainId: chainId,
+    chainId,
   });
 
   const { validator: validatorResponse } = await secretjs.query.staking.validator({validatorAddr: 'secretvaloper18w7rm926ue3nmy8ay58e3lc2nqnttrlhhgpch6'})
-  let scrt = Math.round(parseInt(validatorResponse.tokens) / 1000000 )
-  console.log(`AmberDAO has ${scrt} SCRT staked.`)
+  const scrt = Math.round(parseInt(validatorResponse.tokens) / 1000000 )
+  const { delegationResponses: delegationResponse } = await secretjs.query.staking.validatorDelegations({validatorAddr: 'secretvaloper18w7rm926ue3nmy8ay58e3lc2nqnttrlhhgpch6', pagination: {limit:'10000'}})
+  const total: number = delegationResponse.length
+
   document.querySelector<HTMLElement>('#stake-value').innerHTML=`${scrt}`
   document.querySelector<HTMLElement>('#voting-power').innerHTML=`${scrt}`
 
-  const { delegationResponses: delegationResponse } = await secretjs.query.staking.validatorDelegations({validatorAddr: 'secretvaloper18w7rm926ue3nmy8ay58e3lc2nqnttrlhhgpch6', pagination: {limit:'10000'}})
-  let total: number = delegationResponse.length
-  console.log(`AmberDAO has ${total} delegations.`)
   document.querySelector<HTMLElement>('#delegators').innerHTML=`${total}`
 
 }
