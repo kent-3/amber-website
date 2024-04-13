31a558fbc6f90023126cf9b7c67abea8fa8893df
kent-3 <100624004+kent-3@users.noreply.github.com>
2024-04-13 19:07:36 -0400

    modify base path

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // vite.config.js
 export default {
+    base: "/amber-website/",
     optimizeDeps: {
         esbuildOptions: {
             // Node.js global to browser globalThis
             define: {
-                global: 'globalThis'
+                global: "globalThis",
             },
             // Enable esbuild polyfill plugins
             plugins: [
                 NodeGlobalsPolyfillPlugin({
-                    buffer: true
-                })
-            ]
-        }
-    }
-  }
+                    buffer: true,
+                }),
+            ],
+        },
+    },
+};
